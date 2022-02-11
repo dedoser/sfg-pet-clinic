e01e7b9ad0ff1a6341f46fdc5768d91768ef8c1b
dedoser <closer200021@gmail.com>
2022-02-11 22:00:23 +0300

    #9: add id by creating BaseEntity

--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/Person.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/Person.java
@@ -1,6 +1,6 @@
 package ru.fignigno.sfgpetclinic.data.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/PetType.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/PetType.java
@@ -1,6 +1,6 @@
 package ru.fignigno.sfgpetclinic.data.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
