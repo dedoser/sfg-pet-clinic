d3eb6d67585bba109afedb64af6ee88fa6418e81
dedoser <closer200021@gmail.com>
2022-02-13 14:29:25 +0300

    close #5: implement map based services

--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/CrudService.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/CrudService.java
@@ -11,5 +11,5 @@ public interface CrudService<T, ID> {
 
     void delete(T object);
 
-    void delteById(ID id);
+    void deleteById(ID id);
 }
---
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package ru.fignigno.sfgpetclinic.data.services.map;
+
+import ru.fignigno.sfgpetclinic.data.model.Pet;
+import ru.fignigno.sfgpetclinic.data.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
