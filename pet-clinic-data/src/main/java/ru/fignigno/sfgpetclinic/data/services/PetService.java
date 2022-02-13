fcc59f079a7caba3306fa8ab6b96fb446e539036
dedoser <closer200021@gmail.com>
2022-02-13 13:56:49 +0300

    closes #10: refactor to common crud service interface

--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/PetService.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/PetService.java
@@ -2,12 +2,5 @@ package ru.fignigno.sfgpetclinic.data.services;
 
 import ru.fignigno.sfgpetclinic.data.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-    Pet findById(long id);
-
-    Pet save(Pet Pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/VetService.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/services/VetService.java
@@ -2,13 +2,5 @@ package ru.fignigno.sfgpetclinic.data.services;
 
 import ru.fignigno.sfgpetclinic.data.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(long id);
-
-    Vet save(Vet Vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
