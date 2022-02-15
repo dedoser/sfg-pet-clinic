9f6dafce5c255b8be65e03d61ca21d755afcda79
dedoser <closer200021@gmail.com>
2022-02-15 22:59:46 +0300

    close #28: add properties for owner

--- pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/Owner.java
+++ pet-clinic-data/src/main/java/ru/fignigno/sfgpetclinic/data/model/Owner.java
@@ -7,6 +7,36 @@ public class Owner extends Person {
 
     private Set<Pet> pets;
 
+    private String address;
+
+    private String telephone;
+
+    private String city;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
